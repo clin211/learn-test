9b52df4e7aea20718ca739d3a7be3cbcac517735
Forest <767425412lin@gmail.com>
2022-06-11 21:46:33 +0800

    feat: add eslint and prettier and editorconfig

---
+++ .prettierrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    // 使用tab缩进
+    useTabs: false,
+    // 缩进的空格数
+    tabWidth: 4,
+    // 单行的最大长度, 大于指定数后将自动换行，默认80个字符
+    printWidth: 100,
+    // 使用单引号代替双引号
+    singleQuote: true,
+    // 在多行逗号分隔的句法结构中尽可能打印尾随逗号 <es5|none|all>
+    trailingComma: 'none',
+    // 在对象文字中的括号之间打印空格
+    bracketSpacing: true,
+    // 末尾分号
+    semi: true,
+    // 元素在最后一行的末尾而不是单独在下一行
+    bracketSameLine: false,
+    // 当箭头函数只有一个参数时, 是否包含括号 <always|avoid>  默认: always
+    arrowParens: 'avoid'
+};
