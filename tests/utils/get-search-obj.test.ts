6db047c3ff69be7c13e8fc3b781ecdfb34ecd663
Forest <767425412lin@gmail.com>
2022-06-11 22:28:29 +0800

    refactor: use jest-location-mock lib

--- tests/utils/get-search-obj.test.ts
+++ tests/utils/get-search-obj.test.ts
@@ -2,10 +2,7 @@ import getSearchObj from 'utils/get-search-obj';
 
 describe('getSearchObj', () => {
     it('get the parameters of the current data', () => {
-        // 使用全局暴露出来的 jsdom
-        global.jsdom.reconfigure({
-            url: 'https://www.baidu.com?a=1&b=2'
-        });
+        window.location.assign('https://www.baidu.com?a=1&b=2');
 
         expect(window.location.search).toEqual('?a=1&b=2');
         expect(getSearchObj()).toEqual({
@@ -15,10 +12,7 @@ describe('getSearchObj', () => {
     });
 
     it('return null', () => {
-        // 使用全局暴露出来的 jsdom
-        global.jsdom.reconfigure({
-            url: 'https://www.baidu.com'
-        });
+        window.location.assign('https://www.baidu.com');
 
         expect(window.location.search).toEqual('');
         expect(getSearchObj()).toEqual({});
